6d311d7322fd74865c6f2abb0737f33f72b7cb1b
sutoyuka <sutoyuka@192.168.1.4>
2025-08-20 00:18:42 +0900

    Housecommit0819

--- src/main/java/com/example/app/controller/LoginController.java
+++ src/main/java/com/example/app/controller/LoginController.java
@@ -1,16 +1,18 @@
 package com.example.app.controller;
 
-import jakarta.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.app.domain.User;
 import com.example.app.service.UserService;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -48,7 +50,24 @@ public class LoginController {
         return "signup"; 
     }
     
-   
+    @PostMapping("/registar")
+    public String register(@ModelAttribute @Validated UserForm form,
+    		               BindingResult result,
+    		               RedirectAttributes redirectAttributes) {
+    	if(result.hasErrors()) {
+    		return "signup";
+    		
+    	}
+    	
+    	userService.register(form);
+    	redirectAttributes.addFlashAttribute("message","新規アカウント登録完了しました")
+    	return "redirect:/item/list";
+    }
+    
+    
+    
+    
+    
     @GetMapping("/logout")
     public String logout(HttpSession session) {
         session.invalidate(); 
